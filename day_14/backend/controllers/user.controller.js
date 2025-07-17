db559af59b1451ed50c38c695b60e872e06c08d7
ig-imanish <manishkukran123@gmail.com>
2025-07-17 18:49:40 +0530

    day14

--- day_14/backend/controllers/user.controller.js
+++ day_14/backend/controllers/user.controller.js
@@ -1,5 +1,24 @@
-const userSignup = (req, res) => {
+import { userModel } from "../models/user.model.js";
+
+const userSignup = async (req, res) => {
   const { name, email, password, phoneNumber, age } = req.body;
+
+  try {
+    const user = new userModel({
+      name,
+      email,
+      password,
+      phoneNumber,
+      age,
+    });
+    await user.save();
+    console.log("User signup data:", req.body);
+  } catch (error) {
+    console.error("Error during user signup:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 const userLogin = () => {};
+
+export { userSignup, userLogin };
--- day_14/backend/index.js
+++ day_14/backend/index.js
@@ -2,27 +2,36 @@
 
 // console.log(cowsay.say({ text: "hi" }));
 
-import express from "express";
-import { showUserData } from "./controllers/ShowUserData.js";
-import { updateUserData } from "./controllers/UpdateUserData.js";
-import { createUserData } from "./controllers/CreateUserData.js";
-import { deleteUserData } from "./controllers/DeleteUserData.js";
-import { connection } from "./configs/db.js";
+// import { showUserData } from "./controllers/ShowUserData.js";
+// import { updateUserData } from "./controllers/UpdateUserData.js";
+// import { createUserData } from "./controllers/CreateUserData.js";
+// import { deleteUserData } from "./controllers/DeleteUserData.js";
 // const connection = require("./configs/db.js");
+
+import express from "express";
+import { connection } from "./configs/db.js";
 import { PORT } from "./configs/Config.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
-showUserData(app);
-updateUserData(app);
-createUserData(app);
-deleteUserData(app);
+// Middleware to parse JSON
+app.use(express.json());
+
+// showUserData(app);
+// updateUserData(app);
+// createUserData(app);
+// deleteUserData(app);
+
+app.use(userRoute);
 
 app.listen(PORT, async () => {
   try {
     console.log("DB Connecting...");
     await connection;
     console.log("DB Connected!");
-  } catch {}
+  } catch (error) {
+    console.log("DB Connection Error:", error);
+  }
   console.log(`Server is running on http://localhost:${PORT}`);
 });
